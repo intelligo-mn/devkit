6a9387a2566827f661db1212550bab164e124d2f
Turtuvshin Byambaa <toroo.byamba@gmail.com>
2019-01-03 02:26:57 +0900

    jisho api request

--- lib/main.dart
+++ lib/main.dart
@@ -1,111 +1,135 @@
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+import 'package:share/share.dart';
+import 'package:url_launcher/url_launcher.dart';
 
-void main() => runApp(MyApp());
+void main() => runApp(new MyApp());
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
+    return new MaterialApp(
+      title: 'Memorize',
+      theme: new ThemeData(
+          primarySwatch: Colors.blue,
+          brightness: Brightness.dark
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: new MyHomePage(title: 'Memorize'),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
   MyHomePage({Key key, this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
   final String title;
 
   @override
-  _MyHomePageState createState() => _MyHomePageState();
+  _MyHomePageState createState() => new _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+  var tec = new TextEditingController();
+  final GlobalKey<ScaffoldState> _scaffoldState = GlobalKey<ScaffoldState>();
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
+  _search(String s) async {
+    showDialog(barrierDismissible: false,context: context, child: AlertDialog(content: Row(children: <Widget>[Padding(padding: EdgeInsets.all(10.0),child: CircularProgressIndicator()), Expanded(child: Text("Loading results for\n'"+s+"'"))],)));
+    var q = Uri.encodeFull(s);
+    try{
+      var d = await http.get("https://jisho.org/api/v1/search/words?keyword="+q);
+      if (d.statusCode == 200) {
+        var r = json.decode(d.body);
+
+        if (r["data"].length != 0) {
+          var results = <Widget>[];
+          int i = 0;
+          while (i < r["data"].length){
+            String sharelink = 'https://jisho.org/search/${q}';
+            String definition;
+
+            for (var word in r["data"][i]["senses"]) {
+              definition = word['english_definition'].toString();
+            }
+
+            String jap;
+            String kanji;
+
+            for (var japanese in r["data"][i]["japanese"]) {
+              jap = japanese['reading'];
+              kanji = japanese['word'];
+            }
+
+
+            results.add(Card(
+              child: Column(children: <Widget>[
+                Align(alignment: Alignment.topLeft, child: Text('Meaning: ${jap}', style: TextStyle(fontSize: 25.0),),),
+                Align(alignment: Alignment.topLeft, child: Text('Kanji: ${kanji}', style: TextStyle(fontSize: 25.0))),
+                Align(alignment: Alignment.topLeft, child: Text("Definition: "+definition),),
+                Row(mainAxisAlignment: MainAxisAlignment.end, children: <Widget>[
+                  IconButton(icon: Icon(Icons.link), onPressed: () => Share.share(sharelink)),
+                  IconButton(icon: Icon(Icons.share), onPressed: () => Share.share(kanji+": "+definition+"\n(Source: "+sharelink+")")),
+                ],),
+              ]),
+            ));
+            i++;
+          }
+          Navigator.pop(context);
+          Navigator.push(
+            context,
+            MaterialPageRoute(builder: (context) => ResultScreen(results: results, query: s,)),
+          );
+        }else{_scaffoldState.currentState.showSnackBar(SnackBar(content: Text("No results"), duration: Duration(seconds: 5),));Navigator.pop(context);}
+      }else{_scaffoldState.currentState.showSnackBar(SnackBar(content: Text("Server error"), duration: Duration(seconds: 5),));Navigator.pop(context);}
+    }catch(e){_scaffoldState.currentState.showSnackBar(SnackBar(content: Text("Network error"+e.toString()), duration: Duration(seconds: 5),));Navigator.pop(context);}
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
+    return new Scaffold(
+      key: _scaffoldState,
+      body: new Center(
+        child: new Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
+            Spacer(),
             Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
+              'Memorize',
               style: Theme.of(context).textTheme.display1,
             ),
+            Container(
+              width: MediaQuery.of(context).size.width * 0.75,
+              child: TextField(
+                controller: tec,
+                textAlign: TextAlign.center,
+                keyboardType: TextInputType.text,
+                autofocus: true,
+                onSubmitted: (String s){tec.clear(); _search(s);},
+              ),
+            ),
+            Spacer(),
+            Align(alignment: Alignment.bottomCenter, child: FlatButton(child: Text("Privacy Policy", style: TextStyle(color: Colors.grey),), onPressed: () => showDialog(context: context, child: AlertDialog(title: Text("Privacy Policy"), content: Text("All search requests get sent to https://jisho.org/api/v1/search/words?keyword.\nThis app doesn't collect any data, but the Jisho API may collect data."), actions: <Widget>[FlatButton(child: Text("Urban Dictionary's Privacy Policy"), onPressed: () => launch("https://about.urbandictionary.com/privacy"),), FlatButton(child: Text("Okay"), onPressed: () => Navigator.pop(context),)],),),),),
+            Text(""),
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+}
+
+class ResultScreen extends StatelessWidget {
+  final dynamic results;
+  final String query;
+  ResultScreen({Key key, @required this.results, @required this.query}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Results for: "+query),
+      ),
+      body: Center(
+          child: ListView(children: results,)
+      ),
     );
   }
 }
