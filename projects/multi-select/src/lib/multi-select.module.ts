927a8b67d3d2e30a948923eb531883f9ea841c72
Doljko <doljko927@gmail.com>
2020-01-02 23:25:05 +0800

    updated multi select component

--- projects/multi-select/src/lib/multi-select.module.ts
+++ projects/multi-select/src/lib/multi-select.module.ts
@@ -1,11 +1,14 @@
 import { NgModule } from '@angular/core';
 import { MultiSelectComponent } from './multi-select.component';
-
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 
 @NgModule({
   declarations: [MultiSelectComponent],
   imports: [
+    CommonModule,
+    FormsModule
   ],
   exports: [MultiSelectComponent]
 })
