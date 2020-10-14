b02b808fd3532a43fdbd837a0deb7c4672736746
Turtuvshin Byambaa <tortuvshin.b@chinggis.systems>
2020-10-14 20:21:15 +0800

    created sankey and treemap charts

---
+++ projects/charts/src/lib/colors.ts
@@ -0,0 +1,16 @@
+export enum ChartColors {
+  WEB = "#3aaf85",
+  FACEBOOK = "#3b5998",
+  TWITTER = "#1da1f2",
+  YOUTUBE = "#cd201f",
+  DEFAULT = "#eee",
+
+  POSITIVE = "#2bffc6",
+  NEGATIVE = "#f5365c",
+  UNDEFINED = "#d9d9d9",
+  NEUTRAL = "#11cdef",
+
+  REACH = "#fb6340",
+  VIRALITY = "5603ad",
+  TIME = "#343434",
+}
